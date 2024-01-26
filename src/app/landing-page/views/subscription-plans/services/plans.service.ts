a3c645fadf0239a1416978d7cc1dafa2d11e70df
loveheenavasu <trivediheenalove@gmail.com>
2024-01-26 11:37:58 +0530

    feat:subscription-plan

---
+++ src/app/landing-page/views/subscription-plans/services/plans.service.ts
@@ -0,0 +1,13 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlansService {
+
+  constructor(private http:HttpClient) { }
+
+ 
+}
