8a802a1a3230e8599df1c53445c0d91d1199b3d4
loveheenavasu <trivediheenalove@gmail.com>
2024-02-27 09:20:10 +0530

    fix the issue of authgaurd

--- src/app/shared/utils/guard/auth.guard.ts
+++ src/app/shared/utils/guard/auth.guard.ts
@@ -32,7 +32,7 @@ export class AuthGuard implements CanActivate {
           );
         } else {
            console.log(isAuthenticated,'isAuthenticated')
-          // this.router.navigateByUrl('/login');
+          this.router.navigateByUrl('/login');
           return of(false);
         }
       }),
