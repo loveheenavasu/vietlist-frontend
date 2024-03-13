d99bdaf6fbab22163d867280afd12818b923dc89
loveheenavasu <trivediheenalove@gmail.com>
2024-03-13 16:11:51 +0530

    disable book now section behalf of booking end date

--- src/app/manage-event/components/event-details/event-details.component.ts
+++ src/app/manage-event/components/event-details/event-details.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http'
-import { ActivatedRoute,  Router } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { ChangeDetectorRef, Component, ViewEncapsulation } from '@angular/core'
 import { EventService } from '../../service/event.service'
 import { CommonModule, DatePipe, NgIf, TitleCasePipe } from '@angular/common'
@@ -25,6 +25,7 @@ import { AutocompleteComponent } from 'src/app/shared/utils/googleaddress'
 import { SkeletonLoadingComponent } from 'src/app/common-ui/skeleton-loading/skeleton-loading.component'
 import { DateFilterFn, MatDatepickerModule } from '@angular/material/datepicker'
 import { MatNativeDateModule } from '@angular/material/core'
+import { MatIconModule } from '@angular/material/icon'
 
 // NgxStarRatingModule
 @Component({
@@ -44,15 +45,18 @@ import { MatNativeDateModule } from '@angular/material/core'
     SkeletonLoadingComponent,
     MatDatepickerModule,
     MatNativeDateModule,
-    LoaderComponent
+    LoaderComponent,
+    MatIconModule
   ],
   templateUrl: './event-details.component.html',
   styleUrl: './event-details.component.scss',
   encapsulation: ViewEncapsulation.None,
+  providers: [DatePipe]
 })
 export class EventDetailsComponent {
   public selectedDates: Date[] = [];
-  public booking_date:any = new FormControl('')
+  public isDesabledform: boolean = false
+  public booking_date: any = new FormControl('')
   public number_of_booking = new FormControl('')
   public loader: boolean = false
   public footerPageContent?: any
@@ -101,16 +105,16 @@ export class EventDetailsComponent {
   public ratingMessage: string = '';
   public hoveredRating: number = 0
   public businessListing: boolean = false;
-  public eventPrice : any
+  public eventPrice: any
   public isDateMatched: boolean = false
   public role = Roles
   public term_and_condition = new FormControl('')
-  public convertedBookingDate:any
+  public convertedBookingDate: any
   public date = new Date()
-  public isBookingLoader : boolean = false
+  public isBookingLoader: boolean = false
   public eventNumberOfBooking: any
-  public numberofBookingPrice:any
-  public eventEndDate:any
+  public numberofBookingPrice: any
+  public eventEndDate: any
   /**
    *
    *
@@ -138,7 +142,8 @@ export class EventDetailsComponent {
     private sessionService: AuthenticationService,
     private cd: ChangeDetectorRef,
     private httpClient: HttpClient,
-    private localStorageService: LocalStorageService
+    private localStorageService: LocalStorageService,
+    private datePipe: DatePipe
   ) {
     this.reviewForm = this.fb.group({
       comment_content: ['', Validators.required],
@@ -177,11 +182,11 @@ export class EventDetailsComponent {
       this.isGlobal = res['isGlobal']
     })
 
-    this.number_of_booking.valueChanges.subscribe((res)=>{
-      if(res){
+    this.number_of_booking.valueChanges.subscribe((res) => {
+      if (res) {
         this.numberofBookingPrice = Number(res) * Number(this.eventPrice)
       }
-   
+
     })
   }
 
@@ -325,12 +330,17 @@ export class EventDetailsComponent {
     this.fullPageLoaderService.showLoader()
     this.eventService.getEventDetailsByPostId(this.postId).subscribe({
       next: (res) => {
-        console.log(res, "RESPONSE")
+        console.log(res, 'resresresresres')
+        const currentDate: string = this.datePipe.transform(new Date(), 'yyyy-MM-dd') ?? '';
         const startDate = res.data[0].event_dates?.start_date
         const endDate = res.data[0].event_dates?.end_date
         const startDateNew = this.extractDateFromTimestamp(startDate)
         const endDateNew = this.extractDateFromTimestamp(endDate)
         // this.eventEndDate = this.eventDetails.booking
+        if (currentDate > res?.data[0]?.booking_end_date) {
+          this.isDesabledform = true
+
+        }
         if (startDateNew == endDateNew) {
           this.isDateMatched = true
         } else {
@@ -342,8 +352,8 @@ export class EventDetailsComponent {
         this.overllRating = Number(res.data[0].overall_rating)
           ; (this.latitude = Number(this.eventDetails?.latitude)),
             (this.longitude = Number(this.eventDetails?.longitude))
-            this.eventPrice = this.eventDetails.price
-            this.eventNumberOfBooking = this.eventDetails.number_of_bookings
+        this.eventPrice = this.eventDetails.price
+        this.eventNumberOfBooking = this.eventDetails.number_of_bookings
         this.initMap()
       },
       error: (err) => {
@@ -352,35 +362,35 @@ export class EventDetailsComponent {
     })
   }
 
-  public getnumberOfBooking(){
-    console.log(this.number_of_booking , "nn")
+  public getnumberOfBooking() {
+    console.log(this.number_of_booking, "nn")
   }
   public dateFilter: DateFilterFn<Date | null> = (date: Date | null) => {
     if (date !== null) {
-        const selectedTimestamp = date.getTime(); // Get timestamp of selected date
-        const startDateString = this.eventDetails?.event_dates.start_date;
-        const endDateString = this.eventDetails?.event_dates.end_date;
-        
-        if (startDateString && endDateString) {
-            const startDate = new Date(startDateString);
-            const endDate = new Date(endDateString);
-  
-            const startTime = startDate.getTime(); // Get timestamp of start date
-            const endTime = endDate.getTime(); // Get timestamp of end date
-  
-            if (selectedTimestamp >= startTime && selectedTimestamp <= endTime) {
-                return true; // Date is within the range, enable it
-            } else {
-                console.error('Selected date is not within the range');
-                return false; // Date is not within the range, disable it
-            }
+      const selectedTimestamp = date.getTime(); // Get timestamp of selected date
+      const startDateString = this.eventDetails?.event_dates.start_date;
+      const endDateString = this.eventDetails?.event_dates.end_date;
+
+      if (startDateString && endDateString) {
+        const startDate = new Date(startDateString);
+        const endDate = new Date(endDateString);
+
+        const startTime = startDate.getTime(); // Get timestamp of start date
+        const endTime = endDate.getTime(); // Get timestamp of end date
+
+        if (selectedTimestamp >= startTime && selectedTimestamp <= endTime) {
+          return true; // Date is within the range, enable it
         } else {
-            console.error('Invalid start or end date');
-            return false; // Either start date or end date is invalid, disable it
+          console.error('Selected date is not within the range');
+          return false; // Date is not within the range, disable it
         }
+      } else {
+        console.error('Invalid start or end date');
+        return false; // Either start date or end date is invalid, disable it
+      }
     } else {
-        console.error('Invalid date');
-        return false; // No date selected, disable it
+      console.error('Invalid date');
+      return false; // No date selected, disable it
     }
   };
 
@@ -393,7 +403,7 @@ export class EventDetailsComponent {
       return null; // or handle the case when timestamp is undefined in your specific context
     }
   }
-  
+
 
   public initMap() {
     const mapElement = document.getElementById('map')
@@ -783,8 +793,8 @@ export class EventDetailsComponent {
 
 
 
-  public handleBookingNow(details:any) {
-  console.log(details,'pricepricepricepriceprice')
+  public handleBookingNow(details: any) {
+    console.log(details, 'pricepricepricepriceprice')
     // return
     this.isBookingLoader = true
     if (!this.isAuthentecate) {
@@ -803,24 +813,24 @@ export class EventDetailsComponent {
       // console.log(dataObj , "DATAOBJ")
       const data = {
         event_id: this.eventDetails.post_id,
-        person_id:this.userDetail.user_id,
+        person_id: this.userDetail.user_id,
         booking_price: this.eventDetails.price,
-        booking_date : this.booking_date.value ? this.booking_date.value : this.eventDetails?.event_dates?.start_date,
+        booking_date: this.booking_date.value ? this.booking_date.value : this.eventDetails?.event_dates?.start_date,
         number_of_booking: this.number_of_booking.value
       }
       this.eventService.addEventBooking(data).subscribe({
-        next:(res)=>{
+        next: (res) => {
           this.isBookingLoader = false
-          if(res){
+          if (res) {
             // this.router.navigate(['/target-component'], { queryParams: { ids: ids.join(',') } });
-            this.router.navigate(['/booking-payment/' , details?.price],{queryParams:{eventId:details?.post_id, bookingId:res.booking_detail.booking_id}});
+            this.router.navigate(['/booking-payment/', details?.price], { queryParams: { eventId: details?.post_id, bookingId: res.booking_detail.booking_id } });
           }
         },
-        error:(err)=>{
+        error: (err) => {
           this.isBookingLoader = false
         }
       })
-     
+
     }
   }
 
