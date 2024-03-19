ea580d2ec18f4228a2265c689573de2bfb9ecf66
loveheenavasu <trivediheenalove@gmail.com>
2024-03-19 12:25:03 +0530

    fix the issue of googleaddress

--- src/app/blognews/user-blog-details/user-blog-details.component.ts
+++ src/app/blognews/user-blog-details/user-blog-details.component.ts
@@ -1,7 +1,8 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, Renderer2, RendererStyleFlags2, ViewChild } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef, Component, ElementRef, Renderer2, RendererStyleFlags2, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService, FullPageLoaderService } from '@vietlist/shared';
 import { HomepageService } from 'src/app/landing-page/views/service/homepage.service';
+import { ProfileService } from 'src/app/manage-profile/service/profile.service';
 
 @Component({
   selector: 'app-user-blog-details',
@@ -13,12 +14,29 @@ import { HomepageService } from 'src/app/landing-page/views/service/homepage.ser
 })
 export class UserBlogDetailsComponent {
   @ViewChild('busniessCategoriesSwiper') swiper!: ElementRef
+  @ViewChild('blogSwiper') swiperBlog!: ElementRef
   public blogId: any
   public userdetails: any
   public userBlogDetails: any
   public blogIdtwo: any
-  constructor(private authentication: AuthenticationService, private homeService: HomepageService, private _activatedRoute: ActivatedRoute, private elRef: ElementRef, private renderer: Renderer2, private loaderService: FullPageLoaderService, private router: Router) {
-
+  public addDetail: any[] = []
+  public multipleSpaceId: string[] = []
+  public multipleAdId: string[] = []
+  public ipAddress: any
+  public isAuthenticated: boolean = false;
+  public blogSwiperParams = {
+    slidesPerView: 1,
+    autoplay: {
+      delay: 6000
+    },
+    slidesPreview: 1,
+    on: {
+      init() { },
+    },
+  }
+  constructor(private IpService: ProfileService, private authentication: AuthenticationService, private cdr: ChangeDetectorRef, private homeService: HomepageService, private _activatedRoute: ActivatedRoute, private elRef: ElementRef, private renderer: Renderer2, private loaderService: FullPageLoaderService, private router: Router) {
+    this.isAuthenticated = this.authentication.isAuthenticated()
+    console.log(this.isAuthenticated)
     this._activatedRoute.params.subscribe((res) => {
       this.blogId = res['id']
     })
@@ -99,8 +117,19 @@ export class UserBlogDetailsComponent {
         verified_logo: '/assets/image/cta-verfied-img2.svg',
       },
     ]
-    
+  ngOnInit() {
+    this.showAdBlogPage()
+    this.getIPAdress()
+  }
+
+  public getIPAdress() {
+    this.IpService.getIPAddress().subscribe((res: any) => {
+      this.ipAddress = res.ip
+    })
+  }
   ngAfterViewInit(): void {
+    this.showAdBlogPage()
+    this.cdr.detectChanges();
     const divElement = this.elRef.nativeElement.querySelector('.blog-content');
     const pElements = divElement?.getElementsByTagName('p');
     const h3Elements = divElement?.getElementsByTagName('h3');
@@ -144,9 +173,61 @@ export class UserBlogDetailsComponent {
 
   }
 
+  public myBrowser() {
+    if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
+      return 'Opera';
+    } else if (navigator.userAgent.indexOf("Chrome") != -1) {
+      return 'Chrome';
+    } else if (navigator.userAgent.indexOf("Safari") != -1) {
+      return 'Safari';
+    } else if (navigator.userAgent.indexOf("Firefox") != -1) {
+      return 'Firefox';
+    } else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!(document as any).documentMode == true)) {
+      return 'IE';
+    } else {
+      return 'unknown';
+    }
+  }
+
+  private formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = this.padZero(date.getMonth() + 1);
+    const day = this.padZero(date.getDate());
+    const hours = this.padZero(date.getHours());
+    const minutes = this.padZero(date.getMinutes());
+    const seconds = this.padZero(date.getSeconds());
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
+  private padZero(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
+  public setStats(ad_id?: string, space_id?: string) {
+    const currentDate = new Date();
+    const actionTime = this.formatDate(currentDate);
+    const currentRoute = window.location.href;
+
+    const body = {
+      space_id: space_id ? space_id : this.multipleSpaceId,
+      ad_id: ad_id ? ad_id : this.multipleAdId,
+      action_type: space_id && ad_id ? 'click' : 'view',
+      action_time: actionTime,
+      user_ip: this.ipAddress,
+      browser: this.myBrowser(),
+      page_url: currentRoute
+    }
+    this.homeService.setStats(body).subscribe({
+      next: (res: any) => {
+
+      },
+      error: (err) => {
+
+      }
+    })
+  }
   getUserBlog() {
     this.loaderService.showLoader()
-    this.homeService.userBlogs('10','1').subscribe({
+    this.homeService.userBlogs('10', '1').subscribe({
       next: (res) => {
         if (res) {
           this.loaderService.hideLoader()
@@ -176,11 +257,33 @@ export class UserBlogDetailsComponent {
     })
   }
   viewuserdetails(details: any) {
-
-
     this.router.navigate(['/user-blog-details/', details?.blog_id])
     this.authentication.BlogID.next(details?.blog_id)
     this.getUserBlog()
     this.getUserBlogDetail()
+  }
+
+  goSignup() {
+    console.log('test signup')
+    this.router.navigate(['/register'])
+  }
+  public showAdBlogPage() {
+    this.homeService.showAD().subscribe({
+      next: (res: any) => {
+        const data = res.data.filter((item: any) => item.Page_key === 'blog ad');
+        if (data) {
+          this.addDetail = data[0]?.ads_detail
+          if (this.addDetail) {
+            if (this.swiperBlog && this.swiperBlog.nativeElement) {
+              const swiperEl = this.swiperBlog.nativeElement;
+              Object.assign(swiperEl, this.blogSwiperParams);
+              swiperEl?.initialize();
+            }
+          }
+          this.cdr.detectChanges()
+          console.log(this.addDetail);
+        }
+      }
+    });
   }
 }
