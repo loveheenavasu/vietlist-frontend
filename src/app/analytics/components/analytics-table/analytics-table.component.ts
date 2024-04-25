d4d3dde2611d72353adf8c632f49f7c8442b314f
= <=>
2024-04-25 14:20:15 +0530

    change pop up msg

--- src/app/analytics/components/analytics-table/analytics-table.component.ts
+++ src/app/analytics/components/analytics-table/analytics-table.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { MatSelectModule } from '@angular/material/select'
+import { NgxPaginationModule } from 'ngx-pagination'
 
 @Component({
   selector: 'app-analytics-table',
   standalone: true,
-  imports: [],
+  imports: [NgxPaginationModule, CommonModule, MatSelectModule, FormsModule],
   templateUrl: './analytics-table.component.html',
-  styleUrl: './analytics-table.component.scss'
+  styleUrl: './analytics-table.component.scss',
 })
-export class AnalyticsTableComponent {
+export class AnalyticsTableComponent implements AfterViewInit {
+  @Input() currentPage = 0
+  @Input() totalItems = 0
+  @Input() itemsPerPage = 0
+  @Input() data: any
+  @Input() columns: any
+  @Input() rowAccessor: any
+  @Input() timePeriod: any
+  @Input() paginationHide?: any
+  @Output() handlePageChange = new EventEmitter<any>()
+  @Output() handleTimePeriodChange = new EventEmitter<any>()
+  colspan: any
+  constructor() {}
 
+  handlePageChanges(e: any) {
+    this.handlePageChange.emit(e)
+  }
+  onChange(e: any) {
+    this.handleTimePeriodChange.emit(e)
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.data, 'data')
+    console.log(this.columns, 'columns')
+    console.log(this.rowAccessor, 'rowAccessor')
+    this.colspan = this.columns?.length
+  }
 }
