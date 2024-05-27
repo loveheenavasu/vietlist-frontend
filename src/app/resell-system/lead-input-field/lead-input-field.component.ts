266b295703978945a69d57bf34a78d63bf01901b
= <=>
2024-05-27 15:42:29 +0530

    created file for lead input filed

---
+++ src/app/resell-system/lead-input-field/lead-input-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lead-input-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './lead-input-field.component.html',
+  styleUrl: './lead-input-field.component.scss'
+})
+export class LeadInputFieldComponent {
+
+}
