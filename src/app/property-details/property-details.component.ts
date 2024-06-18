f11b1a933ac83ec941e21e51445615d6a548fafb
= <=>
2024-06-18 15:52:55 +0530

    git push --set-upstream origin property-details-UI

--- src/app/property-details/property-details.component.ts
+++ src/app/property-details/property-details.component.ts
@@ -16,10 +16,23 @@ import { LoaderComponent } from 'src/app/common-ui'
 import { HomepageService } from 'src/app/landing-page/views/service/homepage.service'
 import { ProfileService } from 'src/app/manage-profile/service/profile.service'
 import { PropertyService } from '../property-listings/property.service'
+import {
+  NgbCarousel,
+  NgbCarouselConfig,
+  NgbCarouselModule,
+  NgbSlideEvent,
+  NgbSlideEventSource,
+} from '@ng-bootstrap/ng-bootstrap'
 @Component({
   selector: 'app-property-details',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, CommonModule, LoaderComponent],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    LoaderComponent,
+    NgbCarouselModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './property-details.component.html',
   styleUrl: './property-details.component.scss',
@@ -37,18 +50,19 @@ export class PropertyDetailsComponent {
       'Built in 1958',
       '0.60 Acres',
       '1 Attached garage space',
-      '$188 price/sqft'
+      '$188 price/sqft',
     ],
-    description: 'Welcome to 8514 N 46th... this beautiful home offers a ton of potential, with 1,728 square feet of elegant living space, four spacious bedrooms and two stylish bathrooms. Nestled on a double oversized lot, this property boasts a private pool perfect for cooling off on hot summer days. The backyard is...',
+    description:
+      'Welcome to 8514 N 46th... this beautiful home offers a ton of potential, with 1,728 square feet of elegant living space, four spacious bedrooms and two stylish bathrooms. Nestled on a double oversized lot, this property boasts a private pool perfect for cooling off on hot summer days. The backyard is...',
     agent: {
       name: 'Mike Davis',
-      imageUrl: '' // Replace with actual image URL
+      imageUrl: '', // Replace with actual image URL
     },
     source: {
       name: 'Stellar MLS',
-      mlsId: 'T3530263'
-    }
-  };
+      mlsId: 'T3530263',
+    },
+  }
   public propId: any
   public userdetails: any
   public propertyDetails: any
@@ -58,7 +72,15 @@ export class PropertyDetailsComponent {
   public latitude: any
   public longitude: any
   public propertyImages: any[] = []
+
   @ViewChild('blogSwiper') swiperBlog!: ElementRef
+  paused = false
+  unpauseOnArrow = false
+  pauseOnIndicator = false
+  pauseOnHover = false
+  pauseOnFocus = false
+  isPlaying = true
+  @ViewChild('carousel', { static: true }) carousel!: NgbCarousel
 
   public blogSwiperParams = {
     slidesPerView: 1,
@@ -80,17 +102,64 @@ export class PropertyDetailsComponent {
     private profile: ProfileService,
     private cd: ChangeDetectorRef,
     public propertyService: PropertyService,
+    private carouselConfig: NgbCarouselConfig,
   ) {
     this.isAuthenticated = this.authentication.isAuthenticated()
     this._activatedRoute.params.subscribe((res) => {
       console.log(res, 'response')
       this.propId = res['id']
     })
+    // carouselConfig.showNavigationArrows = false
   }
 
   ngOnInit() {
     if (this.propId) {
       this.getPropertyDetails()
+    }
+  }
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle()
+    } else {
+      this.carousel.pause()
+    }
+    this.paused = !this.paused
+  }
+
+  toggleSlideShow(id: string) {
+    console.log(id, 'id')
+    const imageContainerElements = document.getElementById(id)
+    console.log(imageContainerElements, 'imageContainer')
+    if (!imageContainerElements) return
+    const imageContainer = imageContainerElements as HTMLElement
+
+    this.isPlaying = !this.isPlaying
+    if (this.isPlaying) {
+      imageContainer.style.animationPlayState = 'running'
+      // pauseBtn.textContent = 'Pause';
+    } else {
+      imageContainer.style.animationPlayState = 'paused'
+      // pauseBtn.textContent = 'Play';
+    }
+    this.togglePaused()
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (
+      this.unpauseOnArrow &&
+      slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
+        slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+    ) {
+      this.togglePaused()
+    }
+    if (
+      this.pauseOnIndicator &&
+      !slideEvent.paused &&
+      slideEvent.source === NgbSlideEventSource.INDICATOR
+    ) {
+      this.togglePaused()
     }
   }
 
@@ -134,6 +203,13 @@ export class PropertyDetailsComponent {
 
   public homestatus: any
   public hometype: any
+
+  getHighResolutionImage(imgArr: any[]) {
+    this.propertyImages = imgArr.map((elem) => {
+      return elem.mixedSources.jpeg.pop()
+    })
+  }
+
   public getPropertyDetails() {
     this.loaderService.showLoader()
     this.propertyService.GetPropertyDetails(this.propId).subscribe({
@@ -141,16 +217,16 @@ export class PropertyDetailsComponent {
       // this.profile.getMLSDataById(this.propId).subscribe({  old api to get property details
       next: (res) => {
         if (res) {
-          console.log(res)
+          console.log(res, ',bbkbkbkb')
           this.propertyDetails = res?.data
           // this.propertyImages = [res.data[0]?.imgsrc].flat()
-          this.propertyImages = res.data.big
-          console.log(this.propertyImages , "pppppp")
+          this.getHighResolutionImage(res.data?.photos)
+          console.log(this.propertyImages, 'pppppp')
           const data = this.propertyDetails?.homeStatus?.split('_')
           this.homestatus = data?.join(' ')
           const data2 = this.propertyDetails?.homeType?.split('_')
           this.hometype = data2?.join(' ')
-          console.log(this.homestatus , this.hometype)
+          console.log(this.homestatus, this.hometype)
           this.latitude = Number(this.propertyDetails?.latitude)
           this.longitude = Number(this.propertyDetails?.longitude)
           this.loaderService.hideLoader()
