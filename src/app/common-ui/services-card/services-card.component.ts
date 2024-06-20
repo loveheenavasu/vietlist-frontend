3d76466ceba6a0afe0cc601c9709c1a4e95c5e3f
= <=>
2024-06-20 13:14:40 +0530

    HOME:WIP

--- src/app/common-ui/services-card/services-card.component.ts
+++ src/app/common-ui/services-card/services-card.component.ts
@@ -1,3 +1,4 @@
+import { TextHelperService } from 'src/app/shared/utils/services/textHelper.service'
 import { Component, Input } from '@angular/core'
 
 @Component({
@@ -8,8 +9,11 @@ import { Component, Input } from '@angular/core'
   styleUrl: './services-card.component.scss',
 })
 export class ServicesCardComponent {
+  constructor(public textHelperService: TextHelperService) {}
+
   @Input('bgImg') bgImg: string = '/assets/image/legal.png'
   @Input('title') title: string = 'Legal Services'
   @Input('subTitle') subTitle: string =
     'Navigate the Spectrum of Opportunities: Dive into our diverse business categories to find precisely what you’re seeking, all under the trusted umbrella of Vietlist.biz.'
+  @Input('cardData') cardData: any[] = [1, 2, 3, 4]
 }
